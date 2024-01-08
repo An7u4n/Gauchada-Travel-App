5f3300680a9155bc419636c78c75baee668744e6
An7u4n <miguelitocentu@hotmail.com>
2024-01-07 21:18:00 -0300

    Added Routing and Travel Container

--- src/components/Travels/Card/cardComponent.jsx
+++ src/components/Travels/Card/cardComponent.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TopCard from './topCard';
 import BottomCard from './bottomCard';
+import { Travel } from '../../../models/travel.class';
 
-export default function CardComponent() {
+CardComponent.propTypes = {
+  travel: PropTypes.instanceOf(Travel),
+};
+
+export default function CardComponent({ travel }) {
   return (
     <li className="p-4 max-w-xl bg-white shadow-2xl elevate h-52 rounded-3xl block">
       <TopCard />
